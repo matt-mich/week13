03bc7a5d7c22787b49773d55d521346de8c811bc
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-05-04 16:49:03 -0600

    Finishing review system

--- src/components/movie.js
+++ src/components/movie.js
@@ -33,7 +33,7 @@ class Movie extends Component {
 
     submitReview() {
         const {dispatch} = this.props;
-        dispatch(addReview(this.state.details));
+        dispatch(addReview(this.state));
     }
 
     componentDidMount() {
